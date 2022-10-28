de751b6c5c8a5ee82542b4cfdbf6766c5a47e172
Martyna Fidler <martyna.boo@gmail.com>
2022-10-28 13:23:36 +0200

    Add BottomNavigationBar to homepage

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:my_planner/app/first_page/first_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key? key,
     required this.user,
@@ -11,39 +11,73 @@ class HomePage extends StatelessWidget {
   final User user;
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("My planner"),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('You are logged as ${user.email}'),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    FirebaseAuth.instance.signOut();
-                  },
-                  child: const Text("Log out"),
-                ),
-              ],
-            ),
-            ElevatedButton(
-              child: const Text("Start"),
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (_) => FirstPage(),
+      body: Builder(builder: (context) {
+        if (currentIndex == 0) {
+          return const Center(
+            child: Text("Jeden"),
+          );
+        }
+        if (currentIndex == 1) {
+          return const Center(
+            child: Text("dwa"),
+          );
+        }
+        return Center(
+          child: Column(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('You are logged as ${widget.user.email}'),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      FirebaseAuth.instance.signOut();
+                    },
+                    child: const Text("Log out"),
                   ),
-                );
-              },
-            ),
-          ],
-        ),
+                ],
+              ),
+              ElevatedButton(
+                child: const Text("Start"),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => FirstPage(),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        );
+      }),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (newIndex) {
+          setState(() {
+            currentIndex = newIndex;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month_sharp), label: 'Calendar'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: 'My account'),
+        ],
       ),
     );
   }
